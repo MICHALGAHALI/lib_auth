8b07e25ff664543badf8e33d98484fc9f7aa84ff
michal_gahali <michal6397@gmail.com>
2021-10-03 20:15:28 +0300

    add basic structure project

---
+++ projects/myapp/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { HeaderComponent } from './header/header.component';
+import { CommonModule } from '../common/common.module';
+import { TodoModule } from '../todo/todo.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ projects/myapp/src/todo/todo.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodoListPageComponent } from './todo-list-page/todo-list-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    TodoListPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TodoModule { }
